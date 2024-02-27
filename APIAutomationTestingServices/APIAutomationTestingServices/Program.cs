99dc624572339d4b395f493d90e828826711c510
Mohammad Asif <mohammad.asif@imriel.com>
2024-02-27 16:13:07 +0530

    swagger

--- APIAutomationTestingServices/APIAutomationTestingServices/Program.cs
+++ APIAutomationTestingServices/APIAutomationTestingServices/Program.cs
@@ -31,11 +31,13 @@ var app = builder.Build();
 app.UseCors();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
